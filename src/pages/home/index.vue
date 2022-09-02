ea0bb52ffea7c3e73ef4b6daa98be7491ff77e2e
chenshijie <csjpro0810@163.com>
2022-09-02 15:35:41 +0800

    feat: 增加项目初始首页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,43 +1,45 @@
 <template>
   <button @click="getData">请求</button>
-  <div>{{data?.a}}</div>
-  <div>msg:{{msg}}</div>
-  <Component :msg="msg" @handleClick="handleClick" @update="handleUpdateClick"/>
+  <div>{{ data?.a }}</div>
+  <div>msg:{{ msg }}</div>
+  <Component
+    :msg="msg"
+    @handleClick="handleClick"
+    @update="handleUpdateClick"
+  />
 </template>
 
 <script setup lang="ts">
-import Component from '@/components/Component.vue'
-import globalStore from '@/stores/globalStore';
-import {fetchData} from '@/service/table'
-import { useRequest } from 'vue-request'
+import Component from "@/components/Component.vue";
+import globalStore from "@/stores/globalStore";
+import { fetchData } from "@/service/table";
+import { useRequest } from "vue-request";
 
-const msg = ref<string | number>('oooo')
+const msg = ref<string | number>("oooo");
 
-const {run: fetchTableData,data} = useRequest(fetchData,{
-  manual:true,
-  formatResult:(res) => {
-    msg.value = 'ppp'
-    return res?.data
-  }
-})
+const { run: fetchTableData, data } = useRequest(fetchData, {
+  manual: true,
+  formatResult: (res) => {
+    msg.value = "ppp";
+    return res?.data;
+  },
+});
 const getData = () => {
-  fetchTableData({a:1})
-}
+  fetchTableData({ a: 1 });
+};
 const handleClick = (id: number) => {
-   msg.value = '执行了handleClick'
-  console.log('执行了handleClick',id)
-}
+  msg.value = "执行了handleClick";
+  console.log("执行了handleClick", id);
+};
 
-const handleUpdateClick = (value:string) => {
-  msg.value = '执行了handleUpdateClick'
-  console.log('执行了handleUpdateClick',value)
-}
+const handleUpdateClick = (value: string) => {
+  msg.value = "执行了handleUpdateClick";
+  console.log("执行了handleUpdateClick", value);
+};
 
+const main = globalStore();
 
-const main = globalStore()
-
-console.log(main.counter,main.name)
-
+console.log(main.counter, main.name);
 </script>
 
 <style>
