0eee3288691340f07aed8d8abfb2d72855feced5
chenshijie <csjpro0810@163.com>
2023-04-28 18:09:08 +0800

    feat: 添加todoList链表模式

--- src/pages/Index/components/TodoList/index.vue
+++ src/pages/Index/components/TodoList/index.vue
@@ -1,8 +1,12 @@
 <template>
   <el-form @submit.native.prevent :inline="true" :model="formValue" ref="form">
-    <el-form-item label="代办事项" prop="content">
+    <el-form-item
+      label="代办事项"
+      prop="content"
+      :rules="[{ required: true, message: '请填写' }]"
+    >
       <el-input
-        :style="{ width: '340px' }"
+        :style="{ width: '300px' }"
         v-model="formValue.content"
         placeholder="请输入内容"
       />
@@ -13,28 +17,54 @@
   </el-form>
   <div class="list-container">
     <div v-for="todoItem in todoStore[showType]" class="list-item">
-      <el-checkbox
-        :style="{ width: '80%' }"
-        border
-        :class="todoItem.status ? 'item-done' : ''"
-        :key="todoItem.id"
-        :model-value="todoItem.status"
-        :label="todoItem.content"
-        @change="(val) => statusChange(todoItem.id, val)"
-        @dblclick.capture="changeItemShowType"
-      />
-      <el-button
-        :style="{ marginLeft: '10px' }"
-        :icon="Close"
-        @click="() => todoStore.deleteTodoItem(todoItem.id)"
-      ></el-button>
+      <template :key="todoItem.id" v-if="todoItem.id !== editItemId">
+        <el-checkbox
+          :style="{ width: '80%' }"
+          :border="true"
+          :class="todoItem.status ? 'item-done' : ''"
+          :model-value="todoItem.status"
+          :label="todoItem.content"
+          @change="(val) => statusChange(todoItem.id, val)"
+          @dblclick.capture="changeItemShowType(todoItem)"
+        />
+        <el-button
+          :style="{ marginLeft: '10px' }"
+          :icon="Close"
+          @click="() => todoStore.deleteTodoItem(todoItem.id)"
+        ></el-button>
+      </template>
+      <template v-if="todoItem.id === editItemId">
+        <el-form
+          @submit.native.prevent
+          :inline="true"
+          :model="editFormValue"
+          ref="editForm"
+        >
+          <el-form-item prop="content">
+            <el-input
+              :style="{ width: '305px' }"
+              v-model="editFormValue.content"
+              placeholder="请输入内容"
+            />
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              @click="() => onEditSubmit(todoItem.id, editForm)"
+            >
+              修改
+            </el-button>
+            <el-button @click="editItemId = null"> 取消 </el-button>
+          </el-form-item>
+        </el-form>
+      </template>
     </div>
   </div>
   <div class="sort-container">
     <el-button
-      :type="showType === 'list' ? 'primary' : ''"
-      @click="() => (showType = 'list')"
-      >全部({{ todoStore.list.length }})</el-button
+      :type="showType === 'allList' ? 'primary' : ''"
+      @click="() => (showType = 'allList')"
+      >全部({{ todoStore.allList.length }})</el-button
     >
     <el-button
       :type="showType === 'doneList' ? 'primary' : ''"
@@ -52,20 +82,27 @@
 <script setup lang="ts">
 import { Close } from "@element-plus/icons-vue";
 import useTodoList from "@/stores/useTodoList";
-import { CheckboxValueType, FormInstance } from "element-plus";
+import type { TodoItem } from "@/stores/useTodoList";
+import type { CheckboxValueType, FormInstance } from "element-plus";
 const todoStore = useTodoList();
-const showType = ref<"list" | "doneList" | "notDoneList">("list");
+const showType = ref<"allList" | "doneList" | "notDoneList">("allList");
+const editItemId = ref<number | null>();
 const form = ref<FormInstance>();
+const editForm = ref<FormInstance>();
 const formValue = reactive<{
+  content: string;
+}>({ content: "" });
+const editFormValue = reactive<{
   content: string;
 }>({ content: "" });
 
 const time = 200;
 let timeOut: number;
 
-const changeItemShowType = () => {
+const changeItemShowType = (item: TodoItem) => {
   clearTimeout(timeOut); // 清除第二个单击事件
-  console.log("双击");
+  editItemId.value = item.id;
+  editFormValue.content = item.content;
 };
 
 const statusChange = (id: number, val: CheckboxValueType) => {
@@ -79,6 +116,11 @@ const onSubmit = (form: FormInstance | undefined) => {
   if (!formValue.content) return;
   todoStore.addTodoitem(formValue.content);
   form?.resetFields();
+};
+const onEditSubmit = (id: number, form: FormInstance | undefined) => {
+  if (!editFormValue.content) return;
+  todoStore.editTodoItem(id, editFormValue.content);
+  editItemId.value = null;
 };
 </script>
 
@@ -98,12 +140,12 @@ const onSubmit = (form: FormInstance | undefined) => {
   margin: 0 auto;
 }
 
-::v-deep .el-checkbox__label {
+:deep(.el-checkbox__label) {
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
 }
-::v-deep .item-done .el-checkbox__label {
+:deep(.item-done .el-checkbox__label) {
   text-decoration: line-through;
 }
 </style>
--- src/stores/useTodoList.ts
+++ src/stores/useTodoList.ts
@@ -1,55 +1,154 @@
 import { defineStore } from "pinia";
 
 interface State {
-  list: TodoItem[];
+  list: TodoItem | null;
 }
 
-interface Getter {
-  list: TodoItem[];
-}
-
-interface TodoItem {
+export interface TodoItem {
   id: number;
   status: boolean;
   content: string;
+  next: TodoItem | null;
+  prev: TodoItem | null;
 }
 
 const useTodoList = defineStore("todoList", {
   state: (): State => {
     return {
-      list: [],
+      list: null,
     };
   },
   getters: {
-    doneList: ({ list }) => {
-      return list.filter((item) => item.status);
+    allList: ({ list: all }) => {
+      const _list: TodoItem[] = [];
+      const getAllList = (list: TodoItem | null) => {
+        if (!list?.next) {
+          if (list) {
+            _list.push(list);
+          }
+          return;
+        }
+        _list.push(list);
+        getAllList(list.next);
+      };
+      getAllList(all);
+      return _list;
     },
-    notDoneList: ({ list }) => {
-      return list.filter((item) => !item.status);
+    doneList: ({ list: all }) => {
+      const _list: TodoItem[] = [];
+      const getAllList = (list: TodoItem | null) => {
+        if (!list?.next) {
+          if (list && list.status) {
+            _list.push(list);
+          }
+          return;
+        }
+        if (list.status) {
+          _list.push(list);
+        }
+        getAllList(list.next);
+      };
+      getAllList(all);
+      return _list;
+    },
+    notDoneList: ({ list: all }) => {
+      const _list: TodoItem[] = [];
+      const getAllList = (list: TodoItem | null) => {
+        if (!list?.next) {
+          if (list && !list.status) {
+            _list.push(list);
+          }
+          return;
+        }
+        if (!list.status) {
+          _list.push(list);
+        }
+        getAllList(list.next);
+      };
+      getAllList(all);
+      return _list;
     },
   },
   actions: {
     addTodoitem(content: string) {
-      this.list.push({
-        id: Date.now(),
-        status: false,
-        content,
-      });
+      if (!this.list) {
+        return (this.list = {
+          id: 1,
+          status: false,
+          content,
+          next: null,
+          prev: null,
+        });
+      }
+      const insertLastItem = (list: TodoItem, content: string) => {
+        if (!list.next) {
+          list.next = {
+            id: Date.now(),
+            status: false,
+            content,
+            next: null,
+            prev: list,
+          };
+        } else {
+          insertLastItem(list.next, content);
+        }
+      };
+      insertLastItem(this.list, content);
     },
     deleteTodoItem(id: number) {
-      console.log(id);
-      this.list = this.list.filter((item) => item.id !== id);
+      const deleteItem = (
+        head: TodoItem | null,
+        id: number
+      ): TodoItem | null => {
+        if (head?.id === id) return head.next;
+        let cur = head;
+        let prev = head?.next;
+        while (prev) {
+          if (cur && prev && prev.id === id) {
+            cur.next = prev.next;
+          }
+          cur = cur && cur.next;
+          prev = prev.next;
+        }
+        return head;
+      };
+      this.list = deleteItem(this.list, id);
+    },
+    editTodoItem(id: number, content: string) {
+      const changeItemStatus = (
+        list: TodoItem | null,
+        id: number,
+        content: string
+      ) => {
+        if (list) {
+          if (list.id === id) {
+            return (list.content = content);
+          } else {
+            changeItemStatus(list.next, id, content);
+          }
+        } else {
+          console.log("未找到要修改的项");
+        }
+      };
+      changeItemStatus(this.list, id, content);
     },
     changeStatus(id: number, status: boolean) {
-      this.list = this.list.map((item) => {
-        if (id === item.id) {
-          return {
-            ...item,
-            status: status,
-          };
+      const changeItemStatus = (
+        list: TodoItem | null,
+        id: number,
+        status: boolean
+      ) => {
+        if (list) {
+          if (list.id === id) {
+            return (list.status = status);
+          } else {
+            changeItemStatus(list.next, id, status);
+          }
+        } else {
+          console.log("未找到要修改的项");
         }
-        return item;
-      });
+      };
+      changeItemStatus(this.list, id, status);
     },
   },
 });
